44f353cea37ac0a1419e88710cb7f80772f70674
mech-master <2906465@gmail.com>
2019-09-26 17:46:35 +0300

    Setup popup open and close handlers, setup player characteristics hendler

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,6 @@
 // файл setup.js
 'use strict';
 
-var setupSection = document.querySelector('.setup');
-setupSection.classList.remove('hidden');
-
 var WIZARD_NAMES = ['Иван',
   'Хуан Себастьян',
   'Мария',
@@ -41,10 +38,12 @@ var WIZARD_EYES_COLORS = ['black',
 
 var WIZART_LIST_COUNT = 4;
 
-var getRandomItem = function (featureList) {
+var getRandomItem = function (featureList, cuteCurrent) {
   var currentIndex = Math.round(Math.random() * (featureList.length - 1));
   var currentValue = featureList[currentIndex];
-  featureList.splice(currentIndex, 1);
+  if (cuteCurrent) {
+    featureList.splice(currentIndex, 1);
+  };
   return currentValue;
 };
 
@@ -57,9 +56,9 @@ var generateWisardFeatures = function (names, surnames, coatColors, eyesColors) 
 
   for (var i = 0; i < WIZART_LIST_COUNT; i++) {
     var currentWizardFeatures = {};
-    currentWizardFeatures.name = getRandomItem(generatorWizardNames) + ' ' + getRandomItem(generatorWizardSurnames);
-    currentWizardFeatures.coatColor = getRandomItem(generatorWizardCoatColors);
-    currentWizardFeatures.eyesColor = getRandomItem(generatorWizardEyesColor);
+    currentWizardFeatures.name = getRandomItem(generatorWizardNames, true) + ' ' + getRandomItem(generatorWizardSurnames);
+    currentWizardFeatures.coatColor = getRandomItem(generatorWizardCoatColors, true);
+    currentWizardFeatures.eyesColor = getRandomItem(generatorWizardEyesColor, true);
 
     wisardList.push(currentWizardFeatures);
   }
@@ -90,3 +89,73 @@ var fillWizardList = function (documentFragment, featureList) {
 
 setupSimilarList.appendChild(fillWizardList(fragment, wizardList));
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setupPopup = document.querySelector('.setup');
+var buttonSetupOpen = document.querySelector('.setup-open');
+var buttonSetupClose = setupPopup.querySelector('.setup-close');
+var userNameInput = setupPopup.querySelector('.setup-user-name');
+
+var closePopup = function () {
+  setupPopup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var onPopupEscPress = function (evt) {
+  if ((evt.keyCode === ESC_KEYCODE) && (evt.target !== userNameInput)) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setupPopup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+buttonSetupOpen.addEventListener('click', openPopup);
+
+var buttonSetupOpenIcon = document.querySelector('.setup-open-icon');
+buttonSetupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+buttonSetupClose.addEventListener('click', closePopup);
+buttonSetupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+var WIZARD_FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+var playerCoatColor = document.querySelector('.setup-wizard .wizard-coat');
+playerCoatColor.addEventListener('click', function () {
+  var currentCoatColor = getRandomItem(WIZARD_COAT_COLORS, false);
+  playerCoatColor.style.fill = currentCoatColor;
+  document.querySelector('input[name="coat-color"]').value = currentCoatColor;
+});
+
+var playerEyesColor = document.querySelector('.setup-wizard .wizard-eyes');
+playerEyesColor.addEventListener('click', function () {
+  var currentEyesColor = getRandomItem(WIZARD_EYES_COLORS, false);
+  playerEyesColor.style.fill = currentEyesColor;
+  document.querySelector('input[name="eyes-color"]').value = currentEyesColor;
+});
+
+
+var playerFireballColor = document.querySelector('.setup-fireball-wrap');
+playerFireballColor.addEventListener('click', function () {
+  var currentFireballColor = getRandomItem(WIZARD_FIREBALL_COLORS, false);
+  playerFireballColor.style.backgroundColor = currentFireballColor;
+  document.querySelector('input[name="fireball-color"]').value = currentFireballColor;
+});
